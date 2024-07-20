517e74f4cf70f55a3807d561892d1067d5afe95a
Anjali Roy <anjaliroy3101@gmail.com>
2024-07-20 11:27:50 +0530

    seventh commit

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import "../styles/Experience.scss";
 
 function Experience() {
   return (
-    <div>Experience</div>
-  )
+    <div className="Experience">
+      <div className="Page-Title">
+        <h1>
+          <i>Exp</i>erience
+        </h1>
+      </div>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
